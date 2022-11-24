a71e75b516443bd4d3a98954a8849bd995856108
diogo <diogomaciel.cunha@gmail.com>
2022-11-24 17:09:10 -0300

    implementing circle moviment patterms

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ let gameFrame = 0;
 class Enemy{
 	constructor(){
 		this.image = new Image();
-		this.image.src = "assets/enemies/enemy2.png"
+		this.image.src = "assets/enemies/enemy3.png"
 		this.speed = Math.random() * 5;
-		this.spriteWidth = 266;
-		this.spriteHeight = 188;
+		this.spriteWidth = 218;
+		this.spriteHeight = 177;
 		this.width = this.spriteWidth / 2.5;
 		this.height = this.spriteHeight / 2.5;
 		this.x = Math.random() * (canvas.width - this.width);
 		this.y = Math.random() * (canvas.height - this.height);
 		this.frame = 0;
 		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-		this.angle = 0;
-		this.angleSpeed = Math.random() * 0.2;
-		this.curve = Math.random() * 7;
+		this.angle = Math.random() * 100;
+		this.angleSpeed = Math.random() * 0.5 + 1.5;
+		//this.curve = Math.random() * 200 + 50;
 	}
 	update(){
-		this.x -= this.speed;
-		this.y += this.curve * Math.sin(this.angle);
+		this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/200) + (canvas.width/2 - this.width/2);
+		this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/250) + (canvas.height/2 - this.height/2);
 		this.angle += this.angleSpeed;
 		if (this.x + this.width < 0) this.x = canvas.width;
 		if (gameFrame % this.flapSpeed === 0){
