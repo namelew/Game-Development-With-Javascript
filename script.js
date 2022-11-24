ef2f7a3578af3f7f855c23675153ee2536750f1a
diogo <diogomaciel.cunha@gmail.com>
2022-11-24 16:50:27 -0300

    implementing endless wave moviment in javascript

--- script.js
+++ script.js
@@ -3,27 +3,32 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 10;
+const numberOfEnemies = 50;
 const enemiesArray = []
 let gameFrame = 0;
 
 class Enemy{
 	constructor(){
 		this.image = new Image();
-		this.image.src = "assets/enemies/enemy1.png"
-		//this.speed = Math.random() * 4 - 2;
-		this.spriteWidth = 293;
-		this.spriteHeight = 155;
+		this.image.src = "assets/enemies/enemy2.png"
+		this.speed = Math.random() * 5;
+		this.spriteWidth = 266;
+		this.spriteHeight = 188;
 		this.width = this.spriteWidth / 2.5;
 		this.height = this.spriteHeight / 2.5;
 		this.x = Math.random() * (canvas.width - this.width);
 		this.y = Math.random() * (canvas.height - this.height);
 		this.frame = 0;
 		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+		this.angle = 0;
+		this.angleSpeed = Math.random() * 0.2;
+		this.curve = Math.random() * 7;
 	}
 	update(){
-		this.x += Math.random() * 15 - 7.5;
-		this.y += Math.random() * 15 - 7.5;
+		this.x -= this.speed;
+		this.y += this.curve * Math.sin(this.angle);
+		this.angle += this.angleSpeed;
+		if (this.x + this.width < 0) this.x = canvas.width;
 		if (gameFrame % this.flapSpeed === 0){
 			this.frame > 4 ? this.frame = 0 : this.frame++;
 		}
