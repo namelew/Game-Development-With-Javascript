84056e5c750fff5b2421d46e710cc034d46bd792
diogo <diogomaciel.cunha@gmail.com>
2022-11-24 16:34:41 -0300

    first enemy moveset in javascript

--- script.js
+++ script.js
@@ -1,73 +1,50 @@
+/** @type {HTMLCanvasElement}*/
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 10;
-//let gameFrame = 0;
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 10;
+const enemiesArray = []
+let gameFrame = 0;
 
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = "assets/background/layer-1.png";
-const backgroundLayer2 = new Image();
-backgroundLayer2.src = "assets/background/layer-2.png";
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = "assets/background/layer-3.png";
-const backgroundLayer4 = new Image();
-backgroundLayer4.src = "assets/background/layer-4.png";
-const backgroundLayer5 = new Image();
-backgroundLayer5.src = "assets/background/layer-5.png";
+class Enemy{
+	constructor(){
+		this.image = new Image();
+		this.image.src = "assets/enemies/enemy1.png"
+		//this.speed = Math.random() * 4 - 2;
+		this.spriteWidth = 293;
+		this.spriteHeight = 155;
+		this.width = this.spriteWidth / 2.5;
+		this.height = this.spriteHeight / 2.5;
+		this.x = Math.random() * (canvas.width - this.width);
+		this.y = Math.random() * (canvas.height - this.height);
+		this.frame = 0;
+		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+	}
+	update(){
+		this.x += Math.random() * 15 - 7.5;
+		this.y += Math.random() * 15 - 7.5;
+		if (gameFrame % this.flapSpeed === 0){
+			this.frame > 4 ? this.frame = 0 : this.frame++;
+		}
+	}
+	draw(){
+		ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+	}
+};
 
-window.addEventListener("load", () => {
-	const slider = document.getElementById("slider");
-	slider.value = gameSpeed;
-	const showGameSpeed = document.getElementById("showGameSpeed");
-	showGameSpeed.innerHTML = gameSpeed;
+for(let i = 0; i < numberOfEnemies; i++){
+	enemiesArray.push(new Enemy());
+}
 
-	slider.addEventListener("change", (e) => {
-		gameSpeed = e.target.value;
-		showGameSpeed.innerHTML = e.target.value;
+function animate(){
+	ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+
+	enemiesArray.forEach(enemy => {
+		enemy.update();
+		enemy.draw();
 	});
-	
-	class Layer{
-		constructor(image, speedModifier){
-			this.x = 0;
-			this.y = 0;
-			this.width = 2400;
-			this.height = 700;
-			this.image = image;
-			this.speedModifier = speedModifier;
-			this.speed = gameSpeed * this.speedModifier;
-		}
-
-		update(){
-			this.speed = gameSpeed * this.speedModifier;
-			if(this.x <= -this.width) {
-				this.x = 0;
-			}
-			this.x = Math.floor(this.x - this.speed);
-			// this.x = gameFrame * this.speed % this.width; do the same thing but cause unexpected jumps
-		}
-		draw(){
-			ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-			ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-		}
-	}
-
-	const layer1 = new Layer(backgroundLayer1, 0.2);
-	const layer2 = new Layer(backgroundLayer2, 0.4);
-	const layer3 = new Layer(backgroundLayer3, 0.6);
-	const layer4 = new Layer(backgroundLayer4, 0.8);
-	const layer5 = new Layer(backgroundLayer5, 1);
-
-	const gameObject = [layer1, layer2, layer3, layer4, layer5];
-
-	function animate(){
-		ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-		gameObject.forEach(object => {
-			object.update();
-			object.draw();
-		});
-		//gameFrame--;
-		requestAnimationFrame(animate);
-	}
-	animate();
-})
+	gameFrame++;
+	requestAnimationFrame(animate);
+}
+animate();
